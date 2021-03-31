8ffb404c8bf34a08c181cf5e6dfdf4a5aaee156a
mgosborn <45111373+mgosborn@users.noreply.github.com>
2021-03-30 23:33:35 -0700

    Update Saccharina_microbiome_phenotype_PCA.R

--- Saccharina_microbiome_phenotype_PCA.R
+++ Saccharina_microbiome_phenotype_PCA.R
@@ -59,10 +59,30 @@ temp = transform_sample_counts(physeq,standf)
 
 #Merge samples from multiple lanes
 physeq_merged = merge_samples(temp, "SampleID",fun = mean)
+# For samples with multiple files, divide by number of times sample occurs in dataset
+temp_otu <- t(as.data.frame(physeq_merged@otu_table))
+temp2 <- as.data.frame(table(metadata$SampleID))
+temp2 <- subset(temp2, temp2$Freq>1)
+row.names(temp2) <- temp2$Var1
+for (z in temp2$Var1) {
+  temp_otu[,z] <- round((temp_otu[,z])/temp2[z,"Freq"])
+}
+temp_otu <- t(temp_otu)
+
+OTU = otu_table(temp_otu, taxa_are_rows = FALSE)
+physeq_merged@otu_table = OTU
 sample_data(physeq_merged) <- metadata_merged
 
 #### REMOVE SINGLETONS & DOUBLETONS
 physeq_merged <- prune_taxa(taxa_sums(physeq_merged) > 2, physeq_merged) 
+
+### ALPHA DIVERSITY
+dev.new()
+plot_richness(physeq_merged, x="Select", measures=c("Chao1", "Shannon"))
+
+## BAR PLOT
+dev.new()
+plot_bar(physeq_merged)
 
 
 ##### EXPLORATORY FACTOR ANALYSIS - FactoMineR
@@ -80,15 +100,25 @@ facto_mat <- subset(facto_mat, select = -c(Row.names))
 facto_mat <- na.omit(facto_mat)
 
 ### FactoMineR
-res.pca <- PCA(facto_mat, quali.sup=1228:1230, graph = FALSE)
+res.pca <- PCA(facto_mat, quali.sup=1168:1170, graph = FALSE)
 res.shiny=PCAshiny(res.pca)
-#invest <- Investigate(res.pca, document = "pdf_document")
-save <- dimdesc(res.pca, axes = 1:5)
 
 
+######### Now do the same for only top 50% most abundant OTUs
+PM_top50 = prune_taxa(names(sort(taxa_sums(physeq_merged), TRUE))[1:round(ncol(physeq_merged@otu_table)/2)], physeq_merged)
 
+# center log transform
+PM_top50_transformed <- microbiome::transform(PM_top50, 'clr')
 
+# get otu, tax table, and metadata from physeq
+otumat <- as.data.frame(PM_top50_transformed@otu_table)
 
+# combine otu and metadata - prepare for FactoMineR
+facto_mat <- merge(otumat,sub_metadata, by = 0) # merge by row name
+row.names(facto_mat) <- facto_mat$Row.names
+facto_mat <- subset(facto_mat, select = -c(Row.names))
+facto_mat <- na.omit(facto_mat)
 
-
-
+### FactoMineR
+PM_top50.pca <- PCA(facto_mat, quali.sup=585:587, graph = FALSE)
+PM_top50.shiny=PCAshiny(PM_top50.pca)
